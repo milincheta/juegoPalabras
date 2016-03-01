5bb6ae63a2f408a2bdcf34bbe8f137e9ac6e178b
milincheta <milincheta@hotmail.com>
2016-03-01 10:18:55 -0300

    server API

---
+++ server.js
@@ -0,0 +1,34 @@
+if (Meteor.isServer){
+  // Global API config
+  var Api = new Restivus({
+    //useDefaultAuth: true,
+    prettyJson: true
+  });
+
+  // Get All collection
+  Api.addCollection(Frases);
+
+  // Get by Param
+  Api.addRoute('getByParam/:clave', {authRequired: false}, {
+      get: function () {
+        var palabraClave = this.urlParams.clave;
+        console.log("getByParam, name param is: ", palabraClave);
+        var frases = Frases.findOne({palabraClave: palabraClave});
+        if (frases) {
+          return {
+            statusCode: 200,
+            body: {message: 'Frases con nivel ' + palabraClave, result: frases}
+          };
+        }
+        else {
+          return {
+            statusCode: 404,
+            body: {message: "No se pudo encontrar frases con nivel " + palabraClave}
+          };
+        }
+      },
+      post: function () {
+        console.log("post to customEndpoint");
+      }
+    });
+}
