d3199e5fb62307269e0781a19888fd0421b57d45
milincheta <milincheta@hotmail.com>
2016-03-11 15:59:38 +0000

    no permite agregar palabra clave duplicada

--- juegoFrases.js
+++ juegoFrases.js
@@ -28,8 +28,10 @@ Accounts.ui.config({
       );
 
           //funcion para agregar una frase
-      $scope.agregarFrase =  function(frase) {
-                              $scope.frases.push({frase})
+      $scope.agregarFrase =  function(nuevaFrase) {
+                              var cantidad = Frases.find ({"frase.palabraClave":nuevaFrase.palabraClave}).count()
+                              if (cantidad ===0)
+                              $scope.frases.push({nuevaFrase});
                             };
 
         //funcion para eliminar una frase ya cargada
